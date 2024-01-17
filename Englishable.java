839f2412d26334998f20c40d380a5b5b4151bb0f
yusuke0622 <132802034+yusuke0622@users.noreply.github.com>
2024-01-17 13:52:49 +0000

    Java_interfase

---
+++ Englishable.java
@@ -0,0 +1,6 @@
+public interface Englishable {
+    // 定数(大文字）　（＋public static final)
+    String LANGUAGE = "[英語]";
+    // 抽象メソッド　（＋public abstract）
+    void displayEng();
+}
---
+++ StuSample.java
@@ -0,0 +1,9 @@
+public class StuSample {
+    public static void main(String[] args) {
+        Student stu = new Student("Mike");
+        stu.displayEng();
+        
+        Baseball bb = new Baseball();
+        bb.displayEng();
+    }
+}
